b0f6d416fa15d6d8a92ea42953e6837c12d43d54
AdoptedKitty <65142948+AdoptedKitty@users.noreply.github.com>
2021-02-22 14:17:32 -0500

    Create calculator.py
    basic Python calculator

---
+++ calculator.py
@@ -0,0 +1,44 @@
+user = False
+
+while user == False:
+    user = int(input(
+    """Calculator menu:
+    1- Addition calculator
+    2- Subtraction calculator
+    3- Multiplication calculator
+    4- Division calculator
+    5- Divisibility calculator
+    6- Odd or even calculator"""))
+    if user == 1:
+        a = int(input("Select your first addend"))
+        b = int(input("Select your second addend"))
+        print(a + b)
+    if user == 2:
+        c = int(input("Select the number you're subtracting"))
+        d = int(input("Select the number you're subtracting by"))
+        print(c - d)
+    if user == 3:
+        e = int(input("Select your factor"))
+        f = int(input("Select another factor"))
+        print(e * f) 
+    if user == 4:
+        g = int(input("Select your dividend"))
+        h = int(input("Select your divisor"))
+        print(g * h)
+    if user == 5:
+        i = int(input("Select your dividend"))
+        j = int(input("Select your divisor"))
+        k = i % j
+        if c == 0:
+            print(i, "is divisable by", j)
+        else:
+            print(1, "is not divisable by", j)
+    if user == 6:
+        l = int(input("Select an integer"))
+        m = l % 2
+        if m == 0:
+            print(l, "is even")
+        else:
+            print(l, "is odd")
+
+    user = False
